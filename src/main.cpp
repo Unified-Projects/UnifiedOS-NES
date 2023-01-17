5a3c7846d3468cd04bd8f4deab626552730a9614
Seggy116 <59027059+Seggys116@users.noreply.github.com>
2023-01-17 13:40:40 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,56 @@
 #include <BUS.h>
 
 #include <stdio.h>
+#include <math.h>
+#include <algorithm>
 
 #include <LibUnified/GUI/Window.h>
+#include <LibUnified/Input/Keys.h>
 using namespace LibUnified::GUI;
 using namespace LibUnified::Graphics;
 
+bool Scaled = false;
 surface_t GameWindow;
 surface_t GameWindow2;
 std::shared_ptr<Cartridge> cart;
 Bus* nes;
 Window* window;
+surface_t oldWindow;
+uint64_t SCALE = 1;
+bool Render = true;
 
-#define SCALE 4
+void ScaleNES(surface_t* window){
 
-void ScaleNES(){
+    if(!Scaled || (oldWindow.buffer != window.buffer) /*Resized*/){
+        uint64_t maxXScale = floor(window.width / GameWindow.width);
+        uint64_t maxYScale = floor(window.height / GameWindow.height);
+
+        // Now get minimum of the two
+        SCALE = std::min(maxXScale, maxYScale);
+
+        if(SCALE == 0){
+            // Dissable render as we wont fit
+            Render = false;
+        }
+        else{
+            Render = true;
+        }
+
+        // If scaled delete old data
+        if(Scaled){
+            delete GameWindow2.buffer;
+        }
+
+        // Create new window for scaling
+        GameWindow2 = {};
+        GameWindow2.depth = 32;
+        GameWindow2.width = GameWindow.width * SCALE;
+        GameWindow2.height = GameWindow.height * SCALE;
+        GameWindow2.buffer = new uint8_t[GameWindow2.width * GameWindow2.height * 4];
+
+        oldWindow = window;
+        Scaled = true;
+    }
 
     for(int y = 0; y < GameWindow.height; y++){
         for(int x = 0; x < GameWindow.width; x++){
@@ -30,15 +66,48 @@ void ScaleNES(){
 
 void GameOnDraw(surface_t* window){
     // COPY VIDEOBUFFER TO THE WINDOW
-    ScaleNES();
+    ScaleNES(window);
+
+    if(!Render){
+        return; // Stop issues with blitting a invalid size
+    }
+
     window->Blit(&GameWindow2);
+}
 
-    // memset(window->buffer, 0xFF, window->width * window->height * 4);
-
-    // printf("NES: Window, Size: X: %lu, Y: %lu, Buffer: %lx\n", window->width, window->height, window->buffer);
-    // fflush(stdout);
-
-    // return;
+void GameOnKeyboard(Graphics::surface_t* window, int keyCode, bool Pressed){
+    if(keyCode == "x"){
+        nes.controller[0] &= 0xFF - 0x80;
+        nes.controller[0] |= (Pressed) ? 0x80 : 0x00; // A Button
+    }
+    else if (keyCode == "z"){
+        nes.controller[0] &= 0xFF - 0x40;
+        nes.controller[0] |= (Pressed) ? 0x40 : 0x00; // B Button
+    }
+    else if (keyCode == "a"){
+        nes.controller[0] &= 0xFF - 0x20;
+        nes.controller[0] |= (Pressed) ? 0x20 : 0x00; // Select
+    }
+    else if (keyCode == "s"){
+        nes.controller[0] &= 0xFF - 0x10;
+        nes.controller[0] |= (Pressed) ? 0x10 : 0x00; // Start
+    }
+    else if (keyCode == KEY_UP){
+        nes.controller[0] &= 0xFF - 0x08;
+        nes.controller[0] |= (Pressed) ? 0x08 : 0x00;
+    }
+    else if (keyCode == KEY_DOWN){
+        nes.controller[0] &= 0xFF - 0x04;
+        nes.controller[0] |= (Pressed) ? 0x04 : 0x00;
+    }
+    else if (keyCode == KEY_LEFT){
+        nes.controller[0] &= 0xFF - 0x02;
+        nes.controller[0] |= (Pressed) ? 0x02 : 0x00;
+    }
+    else if (keyCode == KEY_RIGHT){
+        nes.controller[0] &= 0xFF - 0x01;
+        nes.controller[0] |= (Pressed) ? 0x01 : 0x00;
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -79,11 +148,6 @@ int main(int argc, char *argv[]){
     GameWindow.height = nes->ppu.GetScreen().size.y;
     GameWindow.buffer = nes->ppu.GetScreen().buffer;
 
-    // Setup game2 surface
-    GameWindow2.width = nes->ppu.GetScreen().size.x * SCALE;
-    GameWindow2.height = nes->ppu.GetScreen().size.y * SCALE;
-    GameWindow2.buffer = new uint8_t[GameWindow2.width * GameWindow2.height * SCALE];
-
     // Create a window
     printf("NES: Creating Window\n");
     fflush(stdout);
@@ -112,8 +176,8 @@ int main(int argc, char *argv[]){
         // RENDER WINDOW
         window->Render();
 
-        printf("NES: frame\n");
-        fflush(stdout);
+        // printf("NES: frame\n");
+        // fflush(stdout);
     }
 
     return 0;
